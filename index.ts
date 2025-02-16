40461a7a9b06b5dbc23c90d4ea62d7bdadcb9684
Dmitry <d@kozlov.ru>
2025-02-16 23:56:15 +0700

    Added google/auth api

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-
+import { OAuth2Client, type VerifyIdTokenOptions } from 'google-auth-library'
+import { Service } from 'hub-service'
+const client = new OAuth2Client()
+new Service()
+  .post('google/auth', (body: VerifyIdTokenOptions) => client.verifyIdToken(body).then(a => a.getPayload()))
+  .post('google/auth/lite', (body: VerifyIdTokenOptions) => client.verifyIdToken(body).then(a => a.getPayload()?.sub))
+  .start()

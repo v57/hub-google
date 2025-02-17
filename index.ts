ef209030b650281d9b047d78893e383e6a44a1e1
Dmitry <d@kozlov.ru>
2025-02-17 12:12:40 +0700

    Renamed auth to login

--- index.ts
+++ index.ts
@@ -2,6 +2,6 @@ import { OAuth2Client, type VerifyIdTokenOptions } from 'google-auth-library'
 import { Service } from 'hub-service'
 const client = new OAuth2Client()
 new Service()
-  .post('google/auth', (body: VerifyIdTokenOptions) => client.verifyIdToken(body).then(a => a.getPayload()))
-  .post('google/auth/lite', (body: VerifyIdTokenOptions) => client.verifyIdToken(body).then(a => a.getPayload()?.sub))
+  .post('google/login', (body: VerifyIdTokenOptions) => client.verifyIdToken(body).then(a => a.getPayload()))
+  .post('google/login/lite', (body: VerifyIdTokenOptions) => client.verifyIdToken(body).then(a => a.getPayload()?.sub))
   .start()

acd48eb4f7c95809c0805cd07c9211aa9fc5eecb
Dmitry <d@kozlov.ru>
2026-02-11 01:02:56 +0700

    Added service name and icon

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { OAuth2Client, type VerifyIdTokenOptions } from 'google-auth-library'
 import { Service } from 'hub-service'
 const client = new OAuth2Client()
-new Service()
+new Service({ name: 'Login with Google', icon: { text: 'G' } })
   .post('google/login', (body: VerifyIdTokenOptions) => client.verifyIdToken(body).then(a => a.getPayload()))
   .post('google/login/lite', (body: VerifyIdTokenOptions) => client.verifyIdToken(body).then(a => a.getPayload()?.sub))
   .start()
